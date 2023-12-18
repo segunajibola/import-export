8d9d4214824cab56be9e361a96c338818f5e29db
Segun Ajibola <ajbl.segun@gmail.com>
2023-12-19 00:21:28 +0100

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,14 @@
+export const Button = () => {
+  return <button>Click</button>;
+};
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hero Component</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/index.js
@@ -0,0 +1,13 @@
+import { Button } from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Footer from "../components/Footer";
+
+export {
+    Button,
+    Navbar,
+    Hero,
+    About,
+    Footer,
+}
